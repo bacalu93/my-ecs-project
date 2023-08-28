0435beca97555e00612cabd6fe0c6c5f08b9c6c1
bacalu93 <115462674+bacalu93@users.noreply.github.com>
2023-08-28 15:07:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Bitch!"));
+app.get("/", (req, res) => res.send("Hello Nicoleta!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
